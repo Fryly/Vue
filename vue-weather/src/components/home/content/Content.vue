8a2c71353ccd2d38f3aa96925e8a4fa8f5e2623d
Fryly <vaza19max@gmail.com>
2021-12-09 17:54:59 +0300

    feat: add Tabs and dataCity today

--- vue-weather/src/components/home/content/Content.vue
+++ vue-weather/src/components/home/content/Content.vue
@@ -1,22 +1,48 @@
 <template>
   <div class="content-weather">
-    <HeaderContent />
-    <ItemWeather />
+    <HeaderContent
+      :tabs="tabs"
+      @onActiveTab="onActiveTab"
+      :activeTab="activeTab"
+    />
+    <Tabs :tab="activeTab" v-if="dataCity.length !== 0">
+      <TabToday slot="Today" :dataCity="dataCity" />
+      <TabWeek slot="Week" />
+    </Tabs>
+    <div v-else>
+      Введите город
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
 import HeaderContent from "@/components/home/header/HeaderContent.vue";
-import ItemWeather from "@/components/home/cart-item-weather/ItemWeatcher.vue";
+import Tabs from "@/components/tabs/Tabs.vue";
+import TabWeek from "@/components/tabs/TabWeek.vue";
+import TabToday from "@/components/tabs/TabToday.vue";
 
 @Component({
   components: {
     HeaderContent,
-    ItemWeather,
+    Tabs,
+    TabWeek,
+    TabToday,
   },
 })
-export default class Content extends Vue {}
+export default class Content extends Vue {
+  @Prop({ required: true })
+  tabs!: string[];
+
+  @Prop({ required: true })
+  dataCity!: [];
+
+  private activeTab = "Today";
+
+  private onActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
+}
 </script>
 
 <style scoped lang="scss">
